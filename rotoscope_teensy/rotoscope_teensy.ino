f8a6cbe623de7f6c596cac6a104fa127678e5b44
Benjamin Low <Lthben@gmail.com>
2015-01-07 12:43:28 +0800

    working version tested with pot

--- rotoscope_teensy/rotoscope_teensy.ino
+++ rotoscope_teensy/rotoscope_teensy.ino
@@ -1,39 +1,43 @@
-#include <stdio.h>
+/*
+Author: Benjamin Low
+ Date: 7 Jan 2015
+ Description: Simple ADC read from rotoscope
+ by the Teensy 3.1
+ */
 
-#define sensorPin  0//analog pin
-#define ledPin 13//digital pin
+#include <analog.c>
 
-const int numofReadings= 20;
-int arrayReadings[numofReadings];//readings from analog input, average by 20
-int averageSensorValue=0;
-int totalValue=0;
-int index=0;//current index of readings
+#define sensorPin 14
+#define ledPin 6
+
+int sensorReading;
+boolean blinkState; //for the LED just to show circuit is working
 
 void setup() { 
-  Serial.begin(9600);
-  Serial.flush();
-  
-  //initialization
-  for(int i=0; i<numofReadings;i++){
-       arrayReadings[i] = 0;
-  }
-  
-  //Serial.println("start");
-} 
+        Serial.begin(9600);
+        Serial.flush();
+
+        analogReadRes(12); //set to X bit resolution
+        analogReadAveraging(10000); //set to average of X readings
+}
 
 void loop(){
-  // read the value from the sensor:
-  totalValue= totalValue - arrayReadings[index];        
-  arrayReadings[index] = analogRead(sensorPin);
-  totalValue = totalValue + arrayReadings[index];  
-  index = index + 1;
-  if (index >= numofReadings){//already read numofReadings, average it
-    index = 0;
-    averageSensorValue = totalValue/numofReadings;
-    Serial.println(averageSensorValue);
-  }    
-  delay(15); 
-  
+        
+        sensorReading = analogRead(sensorPin);
+        
+        Serial.print("ADC reading: ");
+        Serial.println(sensorReading); 
+        
+        blinkState = !blinkState;
+        
+        if (blinkState) {
+                digitalWrite(ledPin, HIGH);
+        } else {
+                digitalWrite(ledPin, LOW);        
+        }
+        
+        delay(100); 
 }
+
 
 
